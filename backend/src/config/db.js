e8fa55c224359986f904a2850b5e16a5017b0172
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-06-09 16:05:48 +0700

    feature: connect to database on Render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,27 @@
-import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config();
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connect successfully");
-    }catch(error){
-        console.log("Connect fail");
-        process.exit(1);
+import {Pool} from 'pg';
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
     }
-}
-export default connectDB;
+});
+
+// Connect to Database
+(async () => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('PostgreSQL connected at:', result.rows[0].now);
+  } catch (err) {
+    console.error('DB connection failed:', err.message);
+  }
+})();
+
+export default pool;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,7 @@
 import app from '#@/app.js'
-import connectDB from '#@/config/db.js'
-import dotenv from 'dotenv'
-
-dotenv.config();
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log("Hello world");
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
 });
