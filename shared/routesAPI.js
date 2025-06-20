71fe448aa7560f14ca2670099c81685cc6a306bf
Nguyen Duc Tri <ductri0981@gmail.com>
2025-06-20 22:50:31 +0700

    fixbug: bug path of id routesAPI.js

--- shared/routesAPI.js
+++ shared/routesAPI.js
@@ -13,46 +13,85 @@ const API_ROUTES = {
     home: {
         base: HOME_BASE,
         home_page: `${HOME_BASE}/home_page`,
-        highlight_stats_about_us: (id) => `${HOME_BASE}/highlight_stats_about_us/${id}`
+        highlight_stats_about_us: {
+            getAll: `${HOME_BASE}/highlight_stats_about_us`,
+            getOne: (id) => `${HOME_BASE}/highlight_stats_about_us/${id}`,
+        },
     },
     product: {
         base: PRODUCT_BASE,
         product_page: `${PRODUCT_BASE}/product_page`,
-        products: (id) => `${PRODUCT_BASE}/products/${id}`,
-        product_categories: (id) => `${PRODUCT_BASE}/product_categories/${id}`,
-        product_features: (product_id, feature_id) => `${PRODUCT_BASE}/product_features/${product_id}/${feature_id}`,
-        product_highlight_features: (product_id, feature_id) => `${PRODUCT_BASE}/product_highlight_features/${product_id}/${feature_id}`,
-        
+        products: {
+            getAll: `${PRODUCT_BASE}/products`,
+            getOne: (id) => `${PRODUCT_BASE}/products/${id}`,
+        },
+        product_categories: {
+            getAll: `${PRODUCT_BASE}/product_categories`,
+            getOne: (id) => `${PRODUCT_BASE}/product_categories/${id}`,
+        },
+        product_features: {
+            getAll: `${PRODUCT_BASE}/product_features`,
+            getOne: (product_id, feature_id) =>`${PRODUCT_BASE}/product_features/${product_id}/${feature_id}`,
+        },
+        product_highlight_features: {
+            getAll: `${PRODUCT_BASE}/product_highlight_features`,
+            getOne: (product_id, feature_id) => `${PRODUCT_BASE}/product_highlight_features/${product_id}/${feature_id}`,
+        },
+        product_prices: {
+            getAll: `${PRODUCT_BASE}/product_prices`,
+            getOne: (id) => `${PRODUCT_BASE}/product_prices/${id}`,
+        },
         price_page: `${PRODUCT_BASE}/price_page`,
-        product_prices: (id) => `${PRODUCT_BASE}/product_prices/${id}`
     },
     project: {
         base: PROJECT_BASE,
         project_page: `${PROJECT_BASE}/project_page`,
-        projects: (id) => `${PROJECT_BASE}/projects/${id}`,
-        project_regions: (id) => `${PROJECT_BASE}/project_regions/${id}`
+        projects: {
+            getAll: `${PROJECT_BASE}/projects`,
+            getOne: (id) => `${PROJECT_BASE}/projects/${id}`,
+        },
+        project_regions: {
+            getAll: `${PROJECT_BASE}/project_regions`,
+            getOne: (id) => `${PROJECT_BASE}/project_regions/${id}`,
+        },
     },
     news: {
         base: NEWS_BASE,
         news_page: `${NEWS_BASE}/news_page`,
-        news: (id) => `${NEWS_BASE}/news/${id}`,
-        news_categories: (id) => `${NEWS_BASE}/news_categories/${id}`
+        news: {
+            getAll: `${NEWS_BASE}/news`,
+            getOne: (id) => `${NEWS_BASE}/news/${id}`,
+        },
+        news_categories: {
+            getAll: `${NEWS_BASE}/news_categories`,
+            getOne: (id) => `${NEWS_BASE}/news_categories/${id}`,
+        },
     },
     recruitment: {
         base: RECRUITMENT_BASE,
-        recruitment_page: `${RECRUITMENT_BASE}/recruitment_page`
+        recruitment_page: `${RECRUITMENT_BASE}/recruitment_page`,
     },
     contact: {
         base: CONTACT_BASE,
         contact_page: `${CONTACT_BASE}/contact_page`,
         company_info: `${CONTACT_BASE}/company_info`,
-        support_agents: (id) => `${CONTACT_BASE}/support_agents/${id}`,
+        support_agents: {
+            getAll: `${CONTACT_BASE}/support_agents`,
+            getOne: (id) => `${CONTACT_BASE}/support_agents/${id}`,
+        },
     },
     about_us: {
         base: ABOUT_US_BASE,
         about_us_page: `${ABOUT_US_BASE}/about_us_page`,
-        company_services: (id) => `${ABOUT_US_BASE}/company_services/${id}`,
-        why_choose_us: (id) => `${ABOUT_US_BASE}/why_choose_us/${id}`
-    }
-}
-export default API_ROUTES
+        company_services: {
+            getAll: `${ABOUT_US_BASE}/company_services`,
+            getOne: (id) => `${ABOUT_US_BASE}/company_services/${id}`,
+        },
+        why_choose_us: {
+            getAll: `${ABOUT_US_BASE}/why_choose_us`,
+            getOne: (id) => `${ABOUT_US_BASE}/why_choose_us/${id}`,
+        },
+    },
+};
+
+export default API_ROUTES;
