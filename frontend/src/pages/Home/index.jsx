011aaa849d47dde9e4a7b789fd85dee76281e2fb
Minh Luan <luanthuco11@gmail.com>
2025-06-22 18:42:58 +0700

    feat: post categories

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,36 +5,20 @@ import useNews from "@/redux/hooks/useNews"
 import useProducts from "@/redux/hooks/useProducts"
 import useProjects from "@/redux/hooks/useProjects"
 import useRecruitment from "@/redux/hooks/useRecruitment"
-
+import PostCategory from "../../components/PostCategory"
 //Ví dụ chill chill
 export default function Home(){
 
-    const { data: home, isLoading: isLoadingHome } = useHome.getAll();
-    const { data: aboutus, isLoading: isLoadingAboutUs } = useAboutUs.getAll();
-    const { data: contact, isLoading: isLoadingContact } = useContact.getAll();
-    const { data: news, isLoading: isLoadingNews } = useNews.getAll();
-    const {data: product, isLoading: isLoadingProduct} = useProducts.getId(2);
-    const { data: projects, isLoading: isLoadingProjects } = useProjects.getAll();
-    const { data: recruitment, isLoading: isLoadingRecruitment } = useRecruitment.getAll();
-
-    const isLoading = isLoadingHome || isLoadingAboutUs || isLoadingContact ||
-                      isLoadingNews || isLoadingProduct || isLoadingProjects || isLoadingRecruitment;
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-    if (!home || !aboutus || !contact || !news || !product || !projects || !recruitment) {
-        return <p>Dữ liệu chưa sẵn sàng hoặc có lỗi.</p>;
-    }
+    const categories = [
+        'Tất cả',
+        'Miền Bắc',
+        'Miền Trung',
+        'Miền Nam',
+        'Miền Tây',
+    ]
     return (
         <>
-            <h1>Welcome to Home Page</h1>
-            <p>{home.home_page[0].banner_title}</p>
-            <p>{aboutus.about_us_page[0].banner_title}</p>
-            <p>{contact.contact_page[0].banner_title}</p>
-            <p>{news.news_page[0].banner_title}</p>
-            <p>{product.product[0].name}</p>
-            <p>{projects.projects_page[0].banner_title}</p>
-            <p>{recruitment.recruitment_page[0].banner_title}</p>
+            <PostCategory categories= {categories} />
         </>
     )
 }
