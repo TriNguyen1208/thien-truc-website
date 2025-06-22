0ee6c7da229ee298e549146fc2336ec4e81dc614
Nguyen Duc Tri <ductri0981@gmail.com>
2025-06-22 17:43:15 +0700

    fixbug: add nested loop API and remove name of api when getting specific path

--- backend/src/services/aboutus.services.js
+++ backend/src/services/aboutus.services.js
@@ -1,13 +1,13 @@
 import pool from '#@/config/db.js'
 
 const getAllTables = async () => {
-    const about_us_page = await getAboutUsPage();
-    const services = await company_services.getAll();
+    const _about_us_page = await getAboutUsPage();
+    const _company_services = await company_services.getAll();
     const _why_choose_us = await why_choose_us.getAll();
     return {
-        ...about_us_page,
-        ...services,
-        ..._why_choose_us
+        about_us_page: _about_us_page,
+        company_services: _company_services,
+        why_choose_us: _why_choose_us
     };
 }
 
@@ -16,9 +16,7 @@ const getAboutUsPage = async () => {
     if(!about_us_page){
         throw new Error("Can't get about_us_page");
     }
-    return {
-        about_us_page
-    };   
+    return about_us_page;
 }
 
 const company_services = {
@@ -27,18 +25,14 @@ const company_services = {
         if(!company_services){
             throw new Error("Can't get company_services");
         }
-        return {
-            company_services
-        };   
+        return company_services
     },
     getOne: async (id) => {
         const company_service = (await pool.query(`SELECT * FROM about_us.company_services WHERE id = ${id}`)).rows[0];
         if(!company_service){
             throw new Error("Can't get company_services");
         }
-        return {
-            company_service
-        };
+        return company_service;
     }
 }
 
@@ -48,18 +42,14 @@ const why_choose_us = {
         if(!why_choose_us){
             throw new Error("Can't get why_choose_us");
         }
-        return {
-            why_choose_us
-        }; 
+        return why_choose_us
     },
     getOne: async (id) => {
         const why_choose_us = (await pool.query(`SELECT * FROM about_us.why_choose_us WHERE id = ${id}`)).rows[0];
         if(!why_choose_us){
             throw new Error("Can't get why_choose_us");
         }
-        return {
-            why_choose_us
-        }; 
+        return why_choose_us;
     }
 }
 export default { getAllTables, getAboutUsPage, company_services, why_choose_us };
--- backend/src/services/products.services.js
+++ backend/src/services/products.services.js
@@ -1,21 +1,21 @@
 import pool from '#@/config/db.js'
 
 const getAllTables = async () => {
-    const product_page = await getProductPage();
+    const _product_page = await getProductPage();
     const _products = await products.getAll();
     const _product_features = await product_features.getAll();
     const _product_highlight_features = await product_highlight_features.getAll();
     const _product_categories = await product_categories.getAll();
-    const price_page = await getPricePage();
+    const _price_page = await getPricePage();
     const _product_prices = await product_prices.getAll();
     return {
-        ...product_page,
-        ..._products,
-        ..._product_features,
-        ..._product_highlight_features,
-        ..._product_categories,
-        ...price_page,
-        ..._product_prices
+        product_page: _product_page,
+        products: _products,
+        product_features: _product_features,
+        product_highlight_features: _product_highlight_features,
+        product_categories: _product_categories,
+        price_page: _price_page,
+        product_prices: _product_prices
     };
 }
 
@@ -24,29 +24,68 @@ const getProductPage = async () => {
     if(!product_page){
         throw new Error("Can't get product_page");
     }
-    return {
-        product_page
-    };
+    return product_page;
 }
 
 const products = {
     getAll: async () => {
-        const products = (await pool.query("SELECT * FROM product.products")).rows;
-        if(!products){
-            throw new Error("Can't get products");
-        }
-        return {
-            products
-        };
+        const query = `
+            select 
+                prd.id as product_id,
+                prd.name as product_name,
+                prd.description,
+                prd.product_img,
+                prd.warranty_period,
+
+                prd_cate.id as category_id,
+                prd_cate.name as category_name
+
+            from product.products prd
+            join product.product_categories prd_cate on prd.category_id = prd_cate.id
+        `
+        const { rows } = await pool.query(query);
+        const products = rows.map(row => ({
+                id: row.product_id,
+                name: row.product_name,
+                description: row.description,
+                product_img: row.product_img,
+                warranty_period: row.warranty_period,
+                category: {
+                    id: row.category_id,
+                    name: row.category_name
+                }
+            }));
+        return products
     },
     getOne: async (id) => {
-        const product = (await pool.query(`SELECT * FROM product.products WHERE id = ${id}`)).rows[0];
-        if(!product){
-            throw new Error("Can't get products");
-        }
-        return {
-            product
-        };
+        const query = `
+            select 
+                prd.id as product_id,
+                prd.name as product_name,
+                prd.description,
+                prd.product_img,
+                prd.warranty_period,
+
+                prd_cate.id as category_id,
+                prd_cate.name as category_name
+
+            from product.products prd
+            join product.product_categories prd_cate on prd.category_id = prd_cate.id
+            where prd.id = ${id}
+        `
+        const row = (await pool.query(query)).rows[0];
+        const product = {
+                id: row.product_id,
+                name: row.product_name,
+                description: row.description,
+                product_img: row.product_img,
+                warranty_period: row.warranty_period,
+                category: {
+                    id: row.category_id,
+                    name: row.category_name
+                }
+            };
+        return product
     }
 }
 
@@ -56,18 +95,14 @@ const product_categories = {
         if(!product_categories){
             throw new Error("Can't get product_categories");
         }
-        return {
-            product_categories
-        };
+        return product_categories
     },
     getOne: async (id) => {
         const product_category = (await pool.query(`SELECT * FROM product.product_categories WHERE id = ${id}`)).rows[0];
         if(!product_category){
             throw new Error("Can't get product_categories");
         }
-        return {
-            product_category
-        };
+        return product_category
     }
 }
 
@@ -77,27 +112,21 @@ const product_features = {
         if(!product_features){
             throw new Error("Can't get product_features");
         }
-        return {
-            product_features
-        };
+        return product_features
     },
     getByProductId: async (product_id) => {
         const product_features = (await pool.query(`SELECT * FROM product.product_features WHERE product_id = ${product_id}`)).rows;
         if(!product_features){
             throw new Error("Can't get product_features");
         }
-        return {
-            product_features
-        };
+        return product_features
     },
     getOne: async (product_id, feature_id) => {
         const product_feature = (await pool.query(`SELECT * FROM product.product_features WHERE product_id = ${product_id} AND feature_id = ${feature_id}`)).rows[0];
         if(!product_feature){
             throw new Error("Can't get product_features");
         }
-        return {
-            product_feature
-        };
+        return product_feature
     }
 }
 
@@ -107,27 +136,21 @@ const product_highlight_features = {
         if(!product_highlight_features){
             throw new Error("Can't get product_highlight_features");
         }
-        return {
-            product_highlight_features
-        };
+        return product_highlight_features
     },
     getByProductId: async (product_id) => {
         const product_highlight_features = (await pool.query(`SELECT * FROM product.product_highlight_features WHERE product_id = ${product_id}`)).rows;
         if(!product_highlight_features){
             throw new Error("Can't get product_highlight_features");
         }
-        return {
-            product_highlight_features
-        };
+        return product_highlight_features
     },
     getOne: async (product_id, feature_id) => {
         const product_highlight_feature = (await pool.query(`SELECT * FROM product.product_highlight_features WHERE product_id = ${product_id} AND feature_id = ${feature_id}`)).rows[0];
         if(!product_highlight_feature){
             throw new Error("Can't get product_highlight_features");
         }
-        return {
-            product_highlight_feature
-        };
+        return product_highlight_feature
     }
 }
 
@@ -136,31 +159,85 @@ const getPricePage = async () => {
     if(!price_page){
         throw new Error("Can't get price_page");
     }
-    return {
-        price_page
-    };
+    return price_page;
 }
 
 const product_prices = {
     getAll: async () => {
-        const product_prices = (await pool.query("SELECT * FROM product.product_prices")).rows;
-        if(!product_prices){
-            throw new Error("Can't get product_prices");
-        }
-        return {
-            product_prices
-        };
+        const query = `
+            select 
+                prd_pri.id as price_id,
+                prd_pri.price,
+                prd_pri.note,
+
+                prd.id as product_id,
+                prd.name as product_name,
+                prd.description,
+                prd.product_img,
+                prd.warranty_period,
+
+                prd_cate.id as category_id,
+                prd_cate.name as category_name
+            from product.product_prices prd_pri
+            join product.products prd on prd_pri.product_id = prd.id
+            join product.product_categories prd_cate on prd.category_id = prd_cate.id
+        `
+        const { rows } = await pool.query(query);
+        const product_prices = rows.map(row => ({
+            id: row.price_id,
+            price: row.price,
+            note: row.note,
+            product: {
+                id: row.product_id,
+                name: row.product_name,
+                description: row.description,
+                product_img: row.product_img,
+                warranty_period: row.warranty_period,
+                category: {
+                    id: row.category_id,
+                    name: row.category_name
+                }
+            }
+          }));
+        return product_prices
     },
-    getOne: async (product_id) => {
-        const product_price = (await pool.query(`SELECT * FROM product.product_prices WHERE product_id = ${product_id}`)).rows[0];
-        if(!product_price){
-            throw new Error("Can't get products");
-        }
-        return {
-            product_price
-        };
+    getOne: async (id) => {
+        const query = `
+            select 
+                prd_pri.id as price_id,
+                prd_pri.price,
+                prd_pri.note,
+
+                prd.id as product_id,
+                prd.name as product_name,
+                prd.description,
+                prd.product_img,
+                prd.warranty_period,
+
+                prd_cate.id as category_id,
+                prd_cate.name as category_name
+            from product.product_prices prd_pri
+            join product.products prd on prd_pri.product_id = prd.id
+            join product.product_categories prd_cate on prd.category_id = prd_cate.id
+            where prd_pri.id = ${id}
+        `
+        const row = (await pool.query(query)).rows[0];
+        const product_price = {
+            id: row.price_id,
+            price: row.price,
+            note: row.note,
+            product: {
+                id: row.product_id,
+                name: row.product_name,
+                description: row.description,
+                product_img: row.product_img,
+                warranty_period: row.warranty_period,
+                category: {
+                    id: row.category_id,
+                    name: row.category_name
+                }
+            }};
+        return product_price;
     }
 }
-
-
 export default { getAllTables, getProductPage, products, product_categories, product_features, product_highlight_features, getPricePage, product_prices };
