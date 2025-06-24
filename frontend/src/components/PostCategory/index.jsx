f731368c605990d3c6be8449b021b7b0ed653cb7
MinhTri27343 <minhtri26072005@gmai.com>
2025-06-24 22:55:46 +0700

    fix: fix limit  the length of description

--- frontend/src/components/PostCategory/index.jsx
+++ frontend/src/components/PostCategory/index.jsx
@@ -19,7 +19,6 @@ function PostCategory({ categories }) {
     
   return (
     <div onClick={handleParentClick}  className="flex flex-row inline-flex justify-around gap-[8px] p-[8px] shadow-lg rounded-[30px] bg-white w-fit h-[56px]">
-      
        {       
             categories.map((category, index) => {
                return ( <div key = {index} data-index = {index} className={`flex flex-row  px-[24px] py-[12px] rounded-[30px] w-full h-[40px]  cursor-pointer transition-all duration-300 ease-in-out 
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <>
+            <p>Đây là nội dung trang chủ.</p>
+        </>
+    )
+}
