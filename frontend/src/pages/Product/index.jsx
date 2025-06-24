3aac7504d462e096139e343b03d1796e59e4ab7b
Minh Luan <luanthuco11@gmail.com>
2025-06-24 15:05:58 +0700

    feat: product detail

--- frontend/src/pages/Product/index.jsx
+++ frontend/src/pages/Product/index.jsx
@@ -1,14 +1,15 @@
 import useProducts from '@/redux/hooks/useProducts'
+import ProductDetail from '../ProductDetail';
 export default function Product(){
         const { data: product, isLoading: isLoadingProduct } = useProducts.getAll() 
         if (isLoadingProduct) {
             return <p>Loading...</p>;
         }
-       console.log(product)
+       
     return (
         <>
         
-            <p>Đây là nội dung trang sản phẩm.</p>
+            <ProductDetail product={product.product_prices[0].product} price={product.product_prices[0]}/>
         </>
     )
 }
--- frontend/src/pages/ProductDetail/index.jsx
+++ frontend/src/pages/ProductDetail/index.jsx
@@ -1,7 +1,188 @@
-export default function ProductDetail(){
+import {ArrowLeftOutlined} from '@ant-design/icons'
+import  {useState} from 'react'
+function ListPicture({urls, id}){
+
+    return( urls.length != 0?
+        <div className="h-[845px] flex flex-col ">
+                        <div data-index = {id} className="h-[668px] w-full p-[15px] border-[#E5E7EB] border-[1px] rounded-[8px]">
+                            <img data-index = {id}
+                            src={urls[id]} 
+                            alt={''}
+                            className="w-full h-full object-cover rounded-t-[6px]"/>
+
+                        </div>
+                        <div className='h-[150px] grid grid-cols-4 gap-[10px] mt-[10px]'>
+                            {
+                                urls.map((url, index) =>{
+                                    return( id != index ? 
+                                        <div data-index = {index} className='h-[170px] w-[170px] overflow-hidden p-[10px]  border-[#E5E7EB] border-[1px] rounded-[8px] cursor-pointer '>
+                                            <img data-index = {index}
+                                            src={url} 
+                                            alt={''}
+                                            className="w-full h-full object-cover "/>
+                                        </div>
+                                        :(<></>)
+                                        
+                                    )
+                                })
+                            }
+                            
+                        </div>
+                    </div> : (
+                        <div className="h-[668px] w-full bg-gray-200 flex items-center justify-center border border-dashed border-gray-400 rounded-md">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-12 w-12 text-gray-500"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="1.5"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3 5h18M3 19h18M4 7h16v10H4V7zm4 4a2 2 0 100-4 2 2 0 000 4z"
+                            />
+                        </svg>
+                            </div>
+                    )
+     )
+}
+export default function ProductDetail({product , price}){
+    
+    const goBack = ()=>{
+        console.log('Tro ve')
+    }
+    
+    const listPicture = [
+        "https://khoinguonsangtao.vn/wp-content/uploads/2022/09/hinh-anh-gai-xinh-cap-2-3.jpg",
+        'https://tse2.mm.bing.net/th?id=OIP.PthCEIQk0yqgru8L_e4r8gHaJP&pid=Api&P=0&h=180',
+        "https://tse2.mm.bing.net/th?id=OIP.BCnlaNQFI8ofLz6aPHV8fwHaGh&pid=Api&P=0&h=180",
+        "https://www.ldg.com.vn/media/uploads/uploads/27130501-gai-xinh-che-mat-anh-avatar-dep-cho-con-gai-1.jpg",
+        "https://tse4.mm.bing.net/th?id=OIP.Vuy4J9rJfgqqwk_heaKCygHaE8&pid=Api&P=0&h=180",
+    ]
+    const [selectPicture, setSelectPicture] = useState(0);
+    const pickPicture = (e)=>{
+        if(e)
+        {
+            if(e.target.dataset.index)
+            {
+
+                setSelectPicture(e.target.dataset.index)
+            }
+        }
+    }
+    const listSpec = [
+        {
+            atribute: 'Nhà sáng tạo',
+            des: 'Thiên trúc'
+        },
+        {
+            atribute: 'Kích thước',
+            des: 'Thiên trúc'
+        },
+        {
+            atribute: 'Nhà sáng tạo',
+            des: 'Thiên trúc'
+        },
+        {
+            atribute: 'Nhà sáng tạo',
+            des: 'Thiên trúc'
+        },
+        {
+            atribute: 'Nhà sáng tạo',
+            des: 'Thiên trúc'
+        },
+        {
+            atribute: 'Nhà sáng tạo',
+            des: 'Thiên trúc'
+        }
+        
+    ]
+    const listHigtLight =[
+        'Công suất: 45W',
+        'Công suất: 45W',
+        'Công suất: 45W',
+        'Công suất: 45W'
+    ]
     return (
         <>
-            <p>Đây là nội dung trang chi tiết sản phẩm.</p>
+            <div className="container-fluid pt-[30px]">
+                <div className="w-full h-[20px] my-[15x]">
+                    Link
+                </div>
+                <div onClick={goBack} className="flex flex-row w-full h-[20px] my-[15px] gap-[10px] leading-none items-center text-[#14532D] font-medium cursor-pointer">
+                     <ArrowLeftOutlined />
+                    <span>Quay lại danh sách sản phẩm</span>
+                </div>
+                <div className="grid grid-cols-2 grid-rows-2 gap-[30px]">
+                    <div onClick = {pickPicture}>
+                    <ListPicture urls = {listPicture} id = {selectPicture}/>
+                   </div>
+                    <div className="flex flex-col h-[845px]">
+                        <div className="pb-[10px] ">
+                            <span className="font-bold text-[30px] leading-none ">
+                            {product.name}
+                            </span>
+                        </div>
+                        <div className="border-[#E5E7EB] border-y-[1px] py-[15px] line-clamp-1 text-[23px] text-[#ff0000] font-semibold">
+
+                              {`${price.price}` + " ₫"}
+                            
+                        </div>
+                         <div className="py-[10px]">
+                            <span className="text-[16px]">
+                            {product.description}
+                            </span>
+                         </div>
+                    </div>
+                    <div className="h-[566px] p-[20px] border-[#E5E7EB] border-[1px] rounded-[8px]">
+                        <div className="border-l-[5px] border-[#14532D] px-[20px] my-[20px]">
+                            <span className="text-[#14532D] text-[20px] font-bold">
+                                Tính năng nổi bật 
+                            </span>
+                        </div>
+                        <div>
+                            <ul>
+                               {
+                                 listHigtLight.map((hl)=>{
+                                    return(
+                                        <li className='my-[10px]'>
+                                            {hl}
+                                        </li>
+                                    )
+                                })
+                               }
+                            </ul>
+                        </div>
+                    </div>
+                       <div className="h-[566px] p-[20px] border-[#E5E7EB] border-[1px] rounded-[8px]">
+                        <div className="border-l-[5px] border-[#14532D] px-[20px] my-[20px]">
+                            <span className="text-[#14532D] text-[20px] font-bold">
+                               Thông số kỹ thuật
+                            </span>
+                        </div>
+                        <div className="overflow-hidden">
+                            <ul>
+                                {
+                                    listSpec.map((spec,index)=>{
+                                        return(
+                                            <li className={` flex flex-row h-[40px]  items-center p-[10px] my-[5px] ${index %2 == 0? 'bg-[#F9FAFB]': '' }` }>
+                                                    <div className="w-[33%]">{spec.atribute}</div>
+                                                    <div className="w-[67%]">{spec.des}</div>
+                                            </li>
+                                        )
+                                    })
+                                }
+                              
+                               
+                            </ul>
+                        </div>
+                    </div>
+                    
+
+                </div>
+            </div>
         </>
     )
 }
