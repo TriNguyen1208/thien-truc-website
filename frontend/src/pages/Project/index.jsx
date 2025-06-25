2eaf372f5dbcad76113faabd7b47a74f529ae402
MinhTri27343 <minhtri26072005@gmai.com>
2025-06-25 10:47:39 +0700

    feature: create projects pages

--- frontend/src/pages/Project/index.jsx
+++ frontend/src/pages/Project/index.jsx
@@ -1,7 +1,110 @@
-export default function Project(){
+import Banner from "@/components/Banner";
+import ItemPost from "../../components/ItemPost";
+import PostCategory from "../../components/PostCategory";
+import project from "../../services/projects.api"
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+export default function Project() {
+    const handleButton = (category, query) => {
+        console.log(category, query);
+    }
+    const navigate = useNavigate();
+    const [bannerData, setBannerData] = useState(null);
+    const [regionData, setRegionData] = useState(null);
+    const [projectData, setProjectData] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            const dataAll = await project.getAll();
+            console.log(dataAll);
+            setBannerData(dataAll.project_page);
+            setRegionData(dataAll.project_regions);
+            setProjectData(dataAll.projects);
+
+        }
+        fetchData();
+    }, [])
+    console.log(bannerData);
+    console.log(regionData);
+    console.log(projectData);
+    let categoriesData = null;
+    if (regionData != null) {
+        categoriesData = regionData.map(item => item.name);
+        const categoriesDefault = ["Tất cả dự án"];
+        categoriesData = [...categoriesDefault, ...categoriesData];
+        console.log(categoriesData);
+    }
+    const data = {
+        title: bannerData?.banner_title ?? "",
+        description: bannerData?.banner_description ?? "",
+        colorBackground: "var(--gradient-banner)",
+        colorText: "#ffffff",
+        hasSearch: true,
+        categories: categoriesData || ["Tất cả dự án"],
+        contentPlaceholder: "Nhập vào đây",
+        handleButton: handleButton
+    };
+
+    const handleClick1 = (category) => {
+        console.log(category)
+    }
+    const handleClick2 = () => {
+        console.log("hello world");
+    }
+        //Cái này là của whiteButton
+    const handleBannerContact = () => {
+        navigate("/lien-he")
+    }
+    const  bannerContactData = {
+        title: "Sẵn sàng bắt đầu dự án của bạn",
+        description: "Liên hệ với chúng tôi để được tư vấn miễn phí và báo giá chi tiết",
+        colorBackground: "var(--light-green-banner)",
+        colorText: "#000000",
+        hasButton: true,
+        contentButton: "Liên hệ tư vấn",
+        handleButton: handleBannerContact
+    };
+    // return (
+    //     <>
+    //         <Banner data={data}/>
+    //         <p>Đây là trang Home.</p>
+    //     </>
+    // )
     return (
         <>
-            <p>Đây là nội dung trang về dự án.</p>
+            <Banner data={data} />
+            <div className="container-fluid">
+                <div className="my-[40px] text-center">
+                    <h1 className='text-4xl mb-[30px] font-bold'>Công trình tiêu biểu</h1>
+                    <div className="mb-[30px]">
+                        <   PostCategory categories={categoriesData || ["Tất cả dự án"]} handleClick={handleClick1} />
+                    </div>
+                    <div className="inline-block w-1/2 font-[300]">
+                        {bannerData?.banner_description ?? ""}
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-center">
+                    {(projectData || []).map((item, index) => {
+                        item.complete_time = String(item.complete_time)
+                        const dataProject = {
+                            type: 'project',
+                            title: item?.title ?? "",
+                            description: item?.main_content ?? "",
+                            location: item?.province ?? "",
+                            date: item?.complete_time ?? "",
+                            tag: item?.region.name ?? "",
+                            tagColor: item?.region.rgb_color ?? "",
+                            image: item?.main_img ?? "",
+                            handleClick: handleClick2
+                        }
+                        return (
+                            <div key = {index} className="mr-[30px] mb-[30px] w-[437px]">
+                                <ItemPost data={dataProject} />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <Banner data={bannerContactData}/>
         </>
     )
 }
