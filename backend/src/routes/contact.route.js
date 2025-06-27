9bc481d3c1da36c3ab054811c4c395c4dbb134f8
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-06-27 12:14:50 +0700

    fixbug: add package.json

--- backend/src/routes/contact.route.js
+++ backend/src/routes/contact.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import contactController from '#@/controllers/contact.controller.js';
-import validateContact from '#@/middlewares/validateForm.middleware.js';
+import validateForm from '#@/middlewares/validateForm.middleware.js';
 
 const router = express.Router();
 
@@ -9,5 +9,5 @@ router.get('/contact_page', contactController.getContactPage);
 router.get('/company_info', contactController.getCompanyInfo);
 router.get('/support_agents', contactController.support_agents.getAll);
 router.get('/support_agents/:id', contactController.support_agents.getOne);
-router.post('/contact_messages', validateContact, contactController.postContactMessage);
+router.post('/contact_messages', validateForm.validateContact, contactController.postContactMessage);
 export default router;
