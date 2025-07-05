110ed9cc0754bbb0de9e33807accb815b7d7314a
Minh Luan <luanthuco11@gmail.com>
2025-07-05 10:00:52 +0700

    feat: Card

---
+++ admin/src/components/Card/index.jsx
@@ -0,0 +1,31 @@
+
+
+export default function Card({title, description,quanCategory, quanlity, icon})
+{
+    return(<div className="flex flex-col relative w-full h-full p-[24px] border border-[#E5E7EB] rounded-[8px] hover:shadow-md hover:scale-105 transition-all duration-300 ease-in-out">
+        <div className="mb-[6px]">
+            <h1  className = "text-[16px] font-semibold text-black">{title}</h1>
+        </div>
+        <div>
+            <p className=" text-black text-[24px] font-medium " >
+                {quanlity}
+            </p>
+        </div>
+        <div className="flex flex-row">
+              <p className=" text-[#71717A] text-[13px] font-regular " >
+                {description}
+            </p>
+           {
+            quanCategory ?  
+            <p className="ml-[2px] text-[#71717A] text-[13px] font-regular " >
+                {`• ${quanCategory}`}
+            </p> : <></>
+           }
+        </div>
+       {
+        icon ?  <div className="absolute top-[24px] right-[24px]  text-[#71717A] text-[20px]">
+            {icon}
+        </div> : <></>
+       }
+    </div>)
+}
--- admin/src/pages/ControlPanel/index.jsx
+++ admin/src/pages/ControlPanel/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Card from '@/components/Card'
+import {PlayCircleOutlined} from '@ant-design/icons'
 const ControlPanel = () => {
   return (
-    <div>Day la noi dung trang ControlPanel</div>
+
+    <div className='w-[369px] h-[128px]'>
+      <Card title = {"Tổng tin tức"} description = {"Bài viết"} quanlity = {50} quanCategory = {"4 loai"}  icon = {<PlayCircleOutlined />} />
+    </div>
   )
 }
 
