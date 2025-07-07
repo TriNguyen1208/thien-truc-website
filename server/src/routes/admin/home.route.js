86c47f30f368e81c3ebeb8c2132ac1bad03e03ab
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-07-07 15:58:29 +0700

    feature: organize routes folder for admin

--- server/src/routes/admin/home.route.js
+++ server/src/routes/admin/home.route.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import homeController from '#@/controllers/home.controller.js';
+
 const router = express.Router();
+
 router.get('/home_page', homeController.getHomePage);
 
 export default router;
--- server/src/routes/admin/projects.route.js
+++ server/src/routes/admin/projects.route.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 router.get('/', projectsController.getAllTables);
 router.get('/project_page', projectsController.getProjectPage);
-router.get('/projects', projectsController.projects.getList);
-router.get('/projects/:id', projectsController.projects.getOne);
-router.get('/project_regions', projectsController.project_regions.getAll);
-router.get('/project_regions/:id', projectsController.project_regions.getOne);
-router.get('/project_contents', projectsController.project_contents.getAll);
-router.get('/project_contents/:id', projectsController.project_contents.getOne);
+// router.get('/projects', projectsController.projects.getList);
+// router.get('/projects/:id', projectsController.projects.getOne);
+// router.get('/project_regions', projectsController.project_regions.getAll);
+// router.get('/project_regions/:id', projectsController.project_regions.getOne);
+// router.get('/project_contents', projectsController.project_contents.getAll);
+// router.get('/project_contents/:id', projectsController.project_contents.getOne);
 
-router.get('/highlight_projects', projectsController.getHighlightProjects);
-router.get('/search_suggestions', projectsController.getSearchSuggestions);
+// router.get('/highlight_projects', projectsController.getHighlightProjects);
+// router.get('/search_suggestions', projectsController.getSearchSuggestions);
 export default router;
