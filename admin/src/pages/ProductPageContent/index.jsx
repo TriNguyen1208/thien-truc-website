7413aecbc8a0bab7667de8467d2db7b9983ef9d5
Minh Luan <luanthuco11@gmail.com>
2025-07-09 13:38:43 +0700

    havenot done

--- admin/src/pages/ProductPageContent/index.jsx
+++ admin/src/pages/ProductPageContent/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import { useEffect } from 'react'
+import {useLayout} from '@/layouts/LayoutContext'
+import EditBanner from '@/components/EdditBanner'
 const ProductPageContent = () => {
+
+  const {setLayoutProps} = useLayout()
+
+    useEffect(()=>{
+    setLayoutProps({
+      title: "Nội dung Trang sản phẩm",
+      description: "Quản lý nội dung hiển thị trên trang sản phẩm",
+      hasButton: false,
+    })
+  },[])
+  const bannerProps = {
+      title: "Banner Trang sản phẩm",
+      description: "Chỉnh sửa tiêu đề và mô tả banner", 
+      listInput,
+      saveButton
+  }
   return (
-    <div>Day la noi dung trang ProductPageContent</div>
+    <div>
+      <EditBanner/>
+
+    </div>
   )
 }
 
