22b34ceb486923c5bbe5e3815d2274f205fe4ad4
Minh Luan <luanthuco11@gmail.com>
2025-07-09 10:57:45 +0700

    fix: manager

--- admin/src/pages/Manager/index.jsx
+++ admin/src/pages/Manager/index.jsx
@@ -8,6 +8,48 @@ import DynamicForm from '../../components/DynamicForm'
 const Manager = () => {
   const {setLayoutProps} = useLayout();
   const [listManagers , setListManagers] = useState([])
+  const [isModalOpenAdd,setIsModalOpenAdd] = useState(false)
+  const [isModalOpenEdit,setIsModalOpenEdit] = useState(false)
+  //====================================================Start Form=========================================================
+   const handleSubmitButtonAdd = (valueForm) => {
+    console.log('Day la button submit', valueForm)
+    setIsModalOpenAdd(false)
+  }
+   const handleSubmitButtonEdit = (valueForm) => {
+    console.log('Day la button submit', valueForm)
+    setIsModalOpenEdit(false)
+  }
+  const configAdd = {
+    title: "Thêm Manager mới",
+    description: "Điền thông tin để thêm Manager mới",
+    widthModal: 700,
+    contentCancelButton: "Huỷ",
+    contentSubmitButton: "Tạo mới Manager",
+    isModalOpen: isModalOpenAdd,
+    handleSubmitButton: handleSubmitButtonAdd,
+    setIsModalOpen: setIsModalOpenAdd
+  }
+
+  const configEdit = {
+    title: "Chỉnh sửa Manager",
+    description: "Cập nhật thông tin Manager",
+    widthModal: 700,
+    contentCancelButton: "Huỷ",
+    contentSubmitButton: "Cập nhật",
+    isModalOpen: isModalOpenEdit,
+    handleSubmitButton: handleSubmitButtonEdit,
+    setIsModalOpen: setIsModalOpenEdit
+  }
+  const data = [
+    { name: 'username', label: 'Tên đăng nhập', type: 'text', width: 6, isRequired: true, placeholder: "VD: minhtri1503" },
+    { name: 'password', label: 'Mật khẩu', type: 'password', width: 6, isRequired: true},
+    { name: 'name', label: 'Họ tên', type: 'text', width: 12, isRequired: true ,placeholder: "VD: Đỗ Nguyễn Minh Trí" },
+    { name: 'phone', label: 'Số điện thoại', type: 'text', width: 6, isRequired: false ,placeholder: "0123456789" },
+    { name: 'email', label: 'Email', type: 'text', width: 6, placeholder: "VD: minhtri@gmail.com"},
+    { name: 'position', label: 'Vị trí công việc', type: 'text', width: 12, placeholder: "VD: Trưởng phòng kinh doanh"  },
+     { name: 'description', label: 'Mô tả', type: 'textarea', width: 12, isRequired: false, numberRows: 5, maxLength: 200},
+  ]
+  //====================================================End Form=========================================================
   const handleDelItem = (e)=> {
     
     const index = parseInt(e.target.closest("[data-key]").getAttribute("data-key")) ;
@@ -25,10 +67,8 @@ const Manager = () => {
     padding: 4,
     handleButton: (e)=>{
       const index = parseInt(e.target.closest("[data-key]").getAttribute("data-key")) ;
+      setIsModalOpenEdit(true)
       
-      return(<div>
-        <DynamicForm/>
-      </div>)
     }
   }
   const managers = [ 
@@ -77,7 +117,7 @@ const Manager = () => {
       hasButton: true,
       buttonLabel: "Thêm Manager",
       buttonAction:()=>{
-          console.log("Them manager")
+          setIsModalOpenAdd(true)
       }
     })
   },[])
@@ -93,12 +133,14 @@ const Manager = () => {
   }
   
   return (
-    <div> 
+    <div className='bg-white border border-gray-300 rounded-[8px] p-[24px]'> 
       <h1 className='text-black text-[24px] font-semibold my-[12px]'>Danh sách Manager</h1>
-      <p className='text-[#71717A] text-[14px] font-regular  my-[12px]'> Tổng cộng 2 Manager</p>
+      <p className='text-[#71717A] text-[14px] font-regular  my-[12px]'> Tổng cộng {listManagers.length} Manager</p>
       <div>
           <Table {...tableProps}/>
       </div>
+      <DynamicForm data={data} config={configAdd}/>
+      <DynamicForm data={data} config={configEdit}/>
     </div>
   )
 }
