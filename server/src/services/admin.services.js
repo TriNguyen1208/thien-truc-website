3c4c0d4f124028b6c97c8db8e38bc0886febec4c
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-07-09 12:23:22 +0700

    feature: add api getting activity logs

--- server/src/services/admin.services.js
+++ server/src/services/admin.services.js
@@ -14,4 +14,29 @@ const count = async () => {
     return data;
 }
 
-export default { count };
+const activity_logs = {
+    getAll: async() => {
+        const { rows } = (await pool.query(`
+            SELECT log.content, log.username, log.time, acc.fullname
+            FROM admin.activity_logs log
+            JOIN admin.accounts acc ON log.username = acc.username
+            ORDER BY log.time DESC
+            LIMIT 10
+        `));
+
+        if(!rows){
+            throw new Error("Can't get activity_logs or accounts");
+        }
+        
+        const data = rows.map(row => ({
+            content: row.content,
+            username: row.username,
+            fullname: row.fullname,
+            time: row.time
+        }));
+
+        return [...data];
+    }
+}
+
+export default { count, activity_logs };
