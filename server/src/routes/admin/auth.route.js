7b5d3f8ee3a6bd8644724955dc2ec799dbc33534
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-07-13 23:24:54 +0700

    feature: init Token and authentication apis

---
+++ server/src/routes/admin/auth.route.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import authController from '#@/controllers/auth.controller.js';
+import authUtil from '#@/utils/auth.js'
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+router.post('/refresh-token', authController.refreshTokenHandler);
+
+// Test --------------------------------------------------------------
+import authMiddleware from '#@/middlewares/auth.middleware.js';
+const { authenticateToken } = authMiddleware;
+
+router.get('/hashpass/:password', async (req, res) => {
+    const password = req.params.password;
+    const hashedPassword = await authUtil.hashPassword(password);
+    res.json({hashedPassword});
+})
+
+router.get('/login-result', authenticateToken, async (req, res) => {
+    res.json({message: 'Đăng nhập thành công'});
+})
+// -------------------------------------------------------------------
+
+export default router;
