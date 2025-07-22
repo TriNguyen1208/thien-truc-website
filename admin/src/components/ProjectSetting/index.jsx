b78dc68d02c529876531d08d9a08d1691daf67f2
Nguyen Duc Tri <ductri0981@gmail.com>
2025-07-22 22:53:19 +0700

    fix: fix bug underflow word in filter category news, project

--- admin/src/components/ProjectSetting/index.jsx
+++ admin/src/components/ProjectSetting/index.jsx
@@ -24,7 +24,7 @@ const ProjectSetting = ({
         return () => document.removeEventListener('mousedown', handleClickOutside); //go bo su kien event listener. Tranh lap lai nhieu lan
     })
     return (
-        <div className='bg-white p-6 flex flex-col gap-6 rounded-lg shadow-sm border border-gray-200 overflow-x-hidden '>
+        <div className='bg-white p-6 flex flex-col gap-6 rounded-lg shadow-sm border border-gray-200 overflow-visible '>
             <h3 className='text-2xl font-semibold text-[#09090B]'>Cài đặt bài viết</h3>
             <div className='flex flex-col gap-4'>
                 <div className='flex flex-col gap-3 pt-1'>
