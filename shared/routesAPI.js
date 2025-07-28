6f4a4bd697211ec050fea8655452235c5f0560c8
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-07-28 21:27:05 +0700

    fix: getAllFeatured

--- shared/routesAPI.js
+++ shared/routesAPI.js
@@ -55,7 +55,7 @@ const API_ROUTES = {
         project_regions: {
             getAll: `${PROJECT_BASE}/project_regions`,
             getOne: (id) => `${PROJECT_BASE}/project_regions/${id}`,
-            getAllFeatureds: `${PROJECT_BASE}/featured_project_regions`,
+            getAllFeatured: `${PROJECT_BASE}/featured_project_regions`,
         },
         project_contents: {
             getAll: `${PROJECT_BASE}/project_contents`,
