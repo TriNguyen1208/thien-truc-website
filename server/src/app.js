fd28182d6d65cfb2894bca28df215ac3134bb047
root <root@1376695-01.novalocal>
2025-08-05 22:09:04 +0700

    feature: temporarily add test domain

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,28 @@ import { cookie } from 'express-validator';
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    'http://171.244.139.18:3001',
+    'http://171.244.139.18:3002'
+    // thêm domain khi deploy như:
+    'https://yourdomain.com',
+    'http://115.73.3.162:3001',
+    'http://115.73.3.162:3002'
+];
+
 app.use(cors({
-    origin: 'http://localhost:3001', // chỉ định domain frontend
-    credentials: true               // cho phép gửi cookie
+origin: (origin, callback) => {
+    // Cho phép gọi từ Postman hoặc SSR không có origin
+    if (!origin || allowedOrigins.includes(origin)) {
+    return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+},
+credentials: true,
 }));
 
 app.use(express.json());
