c3d03d0072c38dac1eed4c7949bc152a68535f85
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-08-05 22:06:07 +0700

    fix: deploy bugs + (vite) URL

--- server/src/app.js
+++ server/src/app.js
@@ -7,9 +7,24 @@ import { cookie } from 'express-validator';
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://127.0.0.1:3001',
+    'http://127.0.0.1:3002',
+    // thêm domain khi deploy như:
+    'https://yourdomain.com',
+];
+
 app.use(cors({
-    origin: 'http://localhost:3001', // chỉ định domain frontend
-    credentials: true               // cho phép gửi cookie
+origin: (origin, callback) => {
+    // Cho phép gọi từ Postman hoặc SSR không có origin
+    if (!origin || allowedOrigins.includes(origin)) {
+    return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+},
+credentials: true,
 }));
 
 app.use(express.json());
