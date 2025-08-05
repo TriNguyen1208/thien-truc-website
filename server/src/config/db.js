1beaf08b5ab5f9962b0b6eb7bf02c30c070f48a8
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-08-05 16:57:25 +0700

    feature: update ssl in database pool

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -9,9 +9,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    ssl: {
-        rejectUnauthorized: false
-    },
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
     options: '-c search_path="$user",extensions'
 });
 
