61be5329e3eb7e7e10c84a35011b56163951a080
huynhgiaau2112005 <huynhgiaau27112005+ForUniversity@gmail.com>
2025-08-06 14:53:16 +0700

    feature: time out 60s for server

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000;
 import dotenv from 'dotenv'
 dotenv.config({ path: '../.env' });
 
-app.listen(PORT, async () => {
+import http from 'http'
+
+const server = http.createServer(app);
+server.setTimeout(60000);
+
+server.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 });
